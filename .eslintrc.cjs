4abb7dabbbfedfc50d3dd37d4d54d397d92be98a
Oxrial <35791730+Oxrial@users.noreply.github.com>
2023-05-23 14:30:56 +0000

    eslint prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,17 @@
 module.exports = {
-  env: { browser: true, es2020: true },
+  env: {     'browser': true,
+  'es2021': true,
+  'node': true },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended'
   ],
   parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module',  'parser': '@typescript-eslint/parser' },
+  plugins: ['react-refresh', '@typescript-eslint'],
   rules: {
-    'react-refresh/only-export-components': 'warn',
+    'react-refresh/only-export-components': 'warn','prettier/prettier': 'warn'
   },
 }
