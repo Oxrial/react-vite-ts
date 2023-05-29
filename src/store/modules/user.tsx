3d42cd8f8ff9c020c107a1f532250cb254456011
Oxrial <35791730+Oxrial@users.noreply.github.com>
2023-05-29 23:28:29 +0800

    zustand immer

--- src/store/modules/user.tsx
+++ src/store/modules/user.tsx
@@ -1,20 +1,29 @@
 import { getToken } from '@/utils/auth'
 import { create } from 'zustand'
+import { produce } from 'immer'
+
 interface User {
     username: string
+    userpwd: any
 }
 interface UserState {
-    userInfo: User | object
+    userInfo: User | null
     token: () => any
 }
+interface UserAction {
+    login: (user: UserState['userInfo']) => void
+    logout: () => void
+    setUserInfo: (user: UserState['userInfo']) => void
+}
 const userOrigin: UserState = {
-    userInfo: {},
+    userInfo: null,
     token: getToken()
 }
-export const useUser = create((set, get) => ({
+export const useUser = create<UserState & UserAction>(set => ({
     ...userOrigin,
-    login: () => set(() => ({ userInfo: { name: 'INIT' } })),
-    getUserInfo: () => {
-        return get()?.userInfo
-    }
+    setUserInfo: (user: UserState['userInfo']) => set(produce(state => (state.userInfo = user))),
+    login: (user: UserState['userInfo']) => {
+        return user
+    },
+    logout: () => {}
 }))
